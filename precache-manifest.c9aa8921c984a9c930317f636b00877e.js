1bfd8e3b0285131e8891ff207b82eef27264b527
unknown <jordanvillarreal1994@gmail.com>
2020-12-13 17:49:05 -0500

    Updates

---
+++ precache-manifest.c9aa8921c984a9c930317f636b00877e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5dfc1914c2ac2b69a3d9",
+    "url": "/movies-react/static/css/main.51d2ef27.chunk.css"
+  },
+  {
+    "revision": "5dfc1914c2ac2b69a3d9",
+    "url": "/movies-react/static/js/main.5dfc1914.chunk.js"
+  },
+  {
+    "revision": "a90d72dc825d27851565",
+    "url": "/movies-react/static/js/runtime~main.a90d72dc.js"
+  },
+  {
+    "revision": "ea178e36d150dcb3bebc",
+    "url": "/movies-react/static/js/2.ea178e36.chunk.js"
+  },
+  {
+    "revision": "fa6dfa852b3b2aa1bc0864c66ba64d19",
+    "url": "/movies-react/static/media/banner.fa6dfa85.jpg"
+  },
+  {
+    "revision": "99c1800265dca97ac6dafd6ee8e30693",
+    "url": "/movies-react/index.html"
+  }
+];
